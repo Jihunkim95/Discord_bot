e9bedd458eb31d654473e5a366c224b1ddeecf85
Jihunkim95 <darak551@naver.com>
2024-08-12 09:40:43 +0900

    feat: 랜덤 메세지 추가

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 require('dotenv').config();
 const { Client, GatewayIntentBits } = require('discord.js');
 const schedule = require('node-schedule');
+
 // 환경변수
 const token = process.env.DISCORD_TOKEN;
 const channelId = process.env.CHANNEL_ID;
 const roleId = process.env.DISCORD_ROLE_ID;
 
-// 디코 클라이언트
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-// 봇 준비 완료
 client.once('ready', () => {
     console.log('준비완료');
     sendBreakReminder();
@@ -21,6 +20,11 @@ const getEndBreakTime = (hour, minute) => {
     const endBreak = new Date(now.getFullYear(), now.getMonth(), now.getDate(), hour, minute);
     endBreak.setMinutes(endBreak.getMinutes() + 10);
     return endBreak;
+};
+
+// 랜덤 메시지 생성 함수
+const getRandomMessage = (messages) => {
+    return messages[Math.floor(Math.random() * messages.length)];
 };
 
 // 스케줄링 
@@ -50,6 +54,18 @@ async function sendBreakReminder() {
             { hour: 16, minute: 50 },
         ];
 
+        const breakMessages = [
+            `<@&${roleId}> 쉬는 시간입니다! 10분 동안 머리를 식혀봐요^^.:melting_face:`,
+            `<@&${roleId}> 잠시 쉬어가세요! 잠깐이라도 눈을 감아보세요.:zzz:`,
+            `<@&${roleId}> 잠깐의 휴식은 큰 도움이 될 거에요! 스트레칭 한번 해보세요.:muscle:`,
+        ];
+
+        const endBreakMessages = [
+            `<@&${roleId}> 쉬는 시간이 끝났어요 ~!:raised_hands:`,
+            `<@&${roleId}> 다시 일어설 시간입니다! 화이팅!:muscle:`,
+            `<@&${roleId}> 이제 다시 집중할 시간이에요!:alarm_clock:`,
+        ];
+
         times.forEach(time => {
             if (time.hour === 11 && time.minute === 50) {
                 scheduleBreakReminder(
@@ -63,12 +79,12 @@ async function sendBreakReminder() {
                     channel,
                     time.hour,
                     time.minute,
-                    `<@&${roleId}> 쉬는 시간입니다! 10분 동안 머리를 식혀봐요^^.:melting_face:`
+                    getRandomMessage(breakMessages) // 랜덤으로 쉬는 시간 메시지 선택
                 );
 
                 const endBreakTime = getEndBreakTime(time.hour, time.minute);
                 schedule.scheduleJob(endBreakTime, () => {
-                    channel.send(`<@&${roleId}> 쉬는 시간이 끝났어요 ~!:raised_hands:`);
+                    channel.send(getRandomMessage(endBreakMessages)); // 랜덤으로 쉬는 시간 종료 메시지 선택
                 });
             }
         });
@@ -76,6 +92,5 @@ async function sendBreakReminder() {
         console.error('알림 설정 중 오류 발생:', error);
     }
 }
-
 
 client.login(token);
